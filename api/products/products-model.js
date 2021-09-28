df03c20083c39f4e19ec93f9d9718cedd6ba7bd3
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-28 11:36:10 -0400

    added getAllProducts model function

--- api/products/products-model.js
+++ api/products/products-model.js
@@ -1,13 +1,33 @@
 const db = require("../data/db-config");
 
 const getAllProducts = async () => {
-  const soaps = await db("soaps as s")
-    .select("s.*", "i.img")
-    .leftJoin("imgs as i", "i.soap_id", "s.soap_id");
-  return {
-    soaps,
-    giftBoxes: [],
-  };
+  const orders = await db("orders as o")
+    .select("o.order_id", "op.*", "p.price")
+    .join("orders_products as op", "op.order_id", "o.order_id")
+    .join("products as p", "p.product_id", "op.product_id");
+  const categories = await db("categories");
+  const products = await db("products as p");
+
+  products.forEach((prod) => {
+    let sales = 0;
+    let quantitySold = 0;
+    prod.orders = orders.filter((order) => {
+      return order.product_id == prod.product_id;
+    });
+    prod.orders.forEach((order) => {
+      sales = sales + order.price * order.quantity;
+      quantitySold = quantitySold + order.quantity;
+    });
+    prod.quantitySold = quantitySold;
+    prod.sales = sales;
+    delete prod.orders;
+  });
+  categories.forEach((category) => {
+    category.products = products.filter(
+      (prod) => prod.category == category.category_id
+    );
+  });
+  return categories;
 };
 
 module.exports = { getAllProducts };
