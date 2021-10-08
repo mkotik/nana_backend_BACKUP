ca77c0e27ae306b951ded1e99478161aa5b9e161
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-10-08 10:48:01 -0400

    change

--- api/server.js
+++ api/server.js
@@ -10,6 +10,10 @@ server.use(cors());
 
 server.use("/api/products", productsRouter);
 
+server.use("*", (req, res) => {
+  res.status(200).json({ message: "welcome to api" });
+});
+
 server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
